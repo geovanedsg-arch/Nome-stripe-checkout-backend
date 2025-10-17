ff99b85f4c1e8e46c2f03fa19c257f282ba39118
geovanedsg-arch <geovanedsg@gmail.com>
2025-10-16 23:57:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,22 +6,5 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-app.post("/create-checkout-session", async (req, res) => {
-  const { priceId } = req.body;
-
-  try {
-    const session = await stripe.checkout.sessions.create({
-      mode: "subscription",
-      line_items: [{ price: priceId, quantity: 1 }],
-      success_url: "https://SEU-SITE-DA-HORIZONS.app/sucesso",
-      cancel_url: "https://SEU-SITE-DA-HORIZONS.app/cancelado",
-    });
-    res.json({ url: session.url });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.listen(10000, () => console.log("Servidor Stripe ativo na porta 10000"));
+//sk_test_51SDxPy601dGxOPbreccC4JOdTtKXnRgDPuUiFHQNueNDM1Bhn1BAEjUgKrT5L6R4xCPawkBRMo0XnT1W9zNVsXEN0091Apkd5m
+const stripe = new Stripe
